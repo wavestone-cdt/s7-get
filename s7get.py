bd43370f4f54f09aa418cd54da146c26d37e2dd2
Maxime Meignan <maxime.meignan@wavestone.com>
2023-08-14 17:35:21 +0200

    Python3 compatibility and readability rewrite

--- s7get.py
+++ s7get.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 # Copyright (C) 2015, Alexandrine Torrents <alexandrine.torrents at eurecom.fr>
 #                     Mathieu Chevalier <mathieu.chevalier at eurecom.fr>
@@ -11,99 +11,111 @@ GREEN = "\033[32;1m"
 RED = "\033[31;1m"
 CRESET = "\033[0m"
 
-# Parse command line arguments
-parser = argparse.ArgumentParser(description='Read or write on Siemens PLCs.')
-parser.add_argument('-a', metavar='<address>', type=int, nargs=1,
-                   required=True, dest='address',
-                   help='Address from which data will be read/written')
-parser.add_argument('-m', metavar='<mode>', nargs=1, required=True,
-                   dest='mode', choices=['r', 'w'],
-                   help='[r|w] Chosen mode: read or write on the PLC')
-parser.add_argument('-n', metavar='<number>', type=int, nargs=1,
-                   required=True, dest='number',
-                   help='Number of data to read/write')
-parser.add_argument('-d', metavar='<data>', nargs=1, dest='data',
-                   help='Data in binary to write (e.g. 1001)')
-parser.add_argument('ip_address', nargs=1)
-args = parser.parse_args()
+"""
+For testing purposes only
+"""
 
-HOST = args.ip_address[0]  # IP Address
-PORT = 102                 #
-MODE = args.mode[0]        # Read/Write
-ADDRESS = args.address[0]  # Offset in bits
-NUMBER = args.number[0]    # Number of bits to read/write
 
-s7 = snap7.client.Client()
-s7.connect(HOST, 0, 1)
+class MockS7(object):
+    def __init__(self, string):
+        self.content = bytearray(string)
 
-def read():
-    result = []
-    output = []
-    start = int(ADDRESS)/8
-    offset = int(ADDRESS) % 8   # Where the start address is located in the byte
-    offset_bis = 8 - offset       # How many bits are going to be read in the first byte
-    new_number = int(NUMBER) - offset_bis  # New number to calculate the remaining bytes that need to be read
-    if new_number <= 0:
-        size = 1
-    else:
-        size = (new_number-1)/8 + 2
-    s = s7.read_area(snap7.types.areas['PA'], 0, start, size)
-    result = [bits[::-1] for bits in ['0' * (8 - len(bin(x)[2:])) + bin(x)[2:] for x in s]]
-    for k in range(offset, min(8, offset + int(NUMBER))):
-        output.append(result[0][k])  # to write the bits value of the first byte
-        if 8 < offset + int(NUMBER):
-            size_result = len(result)
-            for l in range(1, size_result):
-                for m in range(0, 8):
-                    output.append(result[l][m])  # all the remaining bytes
-    print '===Outputs==='
-    for j in range(0, int(NUMBER)):
-        print "Output " + str(int(ADDRESS) + j) + " : " + output[j]
-    return None 
+    def read_area(self, area, dbnumber, start, size):
+        return self.content[start:][:size]
 
-def write():
-    DATA = args.data[0]        # Data to be written
-    for i in DATA:
-        if i != '0' and i != '1':
-            parser.error("Data to be written shall only contain 0s and 1s")
-    if len(DATA) == 0:
-        parser.error("[-d] parameter misused or absent")
-    if len(DATA) != NUMBER:
-        parser.error("The number of bits to write, specified in [-n], is different from the size of the data given in [-d]")
-    to_be_written = []
-    data = []
-    start = int(ADDRESS)/8
-    offset = int(ADDRESS) % 8   # Where the start address is located in the byte
-    offset_bis = 8-offset       # How many bits are going to be read in the first byte
-    new_number = int(NUMBER) - offset_bis  # New number to calculate the remaining bytes that need to be read
-    if new_number <= 0:
-        size = 1
-    else:
-        size = (new_number-1)/8 + 2
-    to_be_written.append(DATA[:offset_bis])
-    to_be_written += [DATA[i + offset_bis: i + offset_bis + 8] for i in range(0, len(DATA) - offset_bis, 8)]
-    s = s7.read_area(snap7.types.areas['PA'], 0, start, size)
-    read_values = [bits[::-1] for bits in ['0' * (8 - len(bin(x)[2:])) + bin(x)[2:] for x in s]]
-    if offset != 0:
-        data.append(read_values[0][:offset])
-    data += to_be_written[::]
-    if new_number % 8 != 0:
-        data.append(read_values[size - 1][new_number % 8:])
-    if len(data[0]) != 8:
-        data[0:2] = [''.join(data[0:2])]
-    if len(data[-1]) != 8:
-        data[len(data) - 2:len(data)] = [''.join(data[len(data) - 2:len(data)])]
-    data1 = [int(byte[::-1], 2) for byte in data]
-    data = bytearray(data1)
-    s = s7.write_area(snap7.types.areas['PA'], 0, start, data)
-    print GREEN + "[+] " + CRESET  + str(DATA) + " has been written correctly from address " + str(ADDRESS)
-    return None
+    def write_area(self, area, dbnumber, start, data):
+        size = len(data)
+        self.content[start:start + size] = data
+
+    def __repr__(self):
+        return f"MockS7({repr(self.content)})"
+
+
+def read_s7(s7, start_bit, bit_length):
+    """
+    start_bit = 4
+    bit_length = 14
+    |0|1|0|1|1|0|0|1| |0|1|1|1|0|1|0|0| |0|1|0|1|1|1|0|1|
+             ^---start_bit = 4             ^---end_bit = 17 = start_bit + bit_length - 1
+
+    """
+    start_byte = start_bit // 8  # the byte where the first bit is located
+    end_byte = (start_bit + bit_length - 1) // 8  # the byte where the last bit is located
+    byte_length = end_byte - start_byte + 1  # the number of bytes to retrieve
+    s = s7.read_area(snap7.types.areas['PA'], 0, start_byte, byte_length)
+
+    relative_start_bit = start_bit - start_byte * 8  # all subsequent offsets are relative to the start of "s"
+    print('===Outputs===')
+
+    for current_bit_offset in range(relative_start_bit, relative_start_bit + bit_length):
+        current_byte_offset = current_bit_offset // 8  # offset of the byte where the current bit is located
+        bit_offset_in_current_byte = current_bit_offset % 8  # offset of the current bit inside the current byte (0-7)
+        current_bit_value = (s[current_byte_offset] >> bit_offset_in_current_byte) & 1
+        print(f"Output {current_bit_offset}: {current_bit_value}")
+
+
+def write_s7(s7, start_bit, bit_string):
+    """
+    start_bit = 4
+    bit_length = 14
+    |0|1|0|1|1|0|0|1| |0|1|1|1|0|1|0|0| |0|1|0|1|1|1|0|1|
+             ^---start_bit = 4             ^---end_bit = 17 = start_bit + bit_length - 1
+
+    """
+    assert all(i in "01" for i in bit_string)
+    bit_length = len(bit_string)
+
+    start_byte = start_bit // 8  # the byte where the first bit is located
+    end_byte = (start_bit + bit_length - 1) // 8  # the byte where the last bit is located
+    byte_length = end_byte - start_byte + 1  # the number of bytes to retrieve
+    s = s7.read_area(snap7.types.areas['PA'], 0, start_byte, byte_length)
+
+    relative_start_bit = start_bit - start_byte * 8  # all subsequent offsets are relative to the start of "s"
+
+    for current_bit_offset, current_bit_value in zip(range(relative_start_bit, relative_start_bit + bit_length),
+                                                     bit_string):
+        current_byte_offset = current_bit_offset // 8  # offset of the byte where the current bit is located
+        bit_offset_in_current_byte = current_bit_offset % 8  # offset of the current bit inside the current byte (0-7)
+        if current_bit_value == "0":
+            s[current_byte_offset] &= (1 << bit_offset_in_current_byte) ^ 0xFF
+        elif current_bit_value == "1":
+            s[current_byte_offset] |= (1 << bit_offset_in_current_byte)
+        else:
+            assert False
+    s7.write_area(snap7.types.areas['PA'], 0, start_byte, s)
+    print(GREEN + "[+] " + CRESET + str(bit_string) + " has been written correctly from address " + str(start_bit))
+
 
 def main():
-    if MODE == "r":
-        read()
-    elif MODE == "w":
-        write()
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description='Read or write on Siemens PLCs.')
+    parser.add_argument('-a', metavar='<address>', type=int, required=True, dest='address',
+                        help='Address from which data will be read/written')
+    parser.add_argument('-m', metavar='<mode>', required=True, dest='mode', choices=['r', 'w'],
+                        help='[r|w] Chosen mode: read or write on the PLC')
+    parser.add_argument('-n', metavar='<number>', type=int, dest='number',
+                        help='Number of data to read/write')
+    parser.add_argument('-d', metavar='<data>', type=str, dest='data',
+                        help='Data in binary to write (e.g. 1001)')
+    parser.add_argument('ip_address', type=str)
+    args = parser.parse_args()
+
+    s7 = snap7.client.Client()
+    s7.connect(args.ip_address, 0, 1)
+    # s7 = MockS7("Lorem ipsum".encode())
+
+    if args.mode == "r":
+        if not args.number:
+            parser.error("-n parameter misused or absent")
+        read_s7(s7, args.address, args.number)
+    elif args.mode == "w":
+        data = args.data
+        if len(data) == 0:
+            parser.error("[-d] parameter misused or absent")
+        if not all(i in "01" for i in data):
+            parser.error("Data to be written shall only contain 0s and 1s")
+        write_s7(s7, args.address, data)
+
 
 if __name__ == "__main__":
     main()
